e1f0db41ff23ffe5e10dab60ff90142dd2ba4718
Anand Balakrishnan <anandbala1597@gmail.com>
2021-04-10 01:00:29 -0700

    move monitors here

---
+++ percemon_ros_example/perception_monitor/src/monitor.cc
@@ -0,0 +1,131 @@
+// ROS stuff
+#include <rclcpp/rclcpp.hpp>
+#include <rcutils/logging_macros.h>
+
+// Darknet messages
+#include <perception_interfaces/msg/bounding_box.hpp>
+#include <perception_interfaces/msg/bounding_boxes.hpp>
+
+// Monitor
+#include <monitoring_interfaces/msg/satisfaction.hpp>
+#include <monitoring_interfaces/msg/satisfaction_array.hpp>
+
+#include <percemon/percemon.hpp>
+
+#include <std_msgs/msg/header.hpp>
+
+#include <array>
+#include <memory>
+#include <random>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <functional>
+
+namespace {
+
+constexpr double IMG_WIDTH  = 1224;
+constexpr double IMG_HEIGHT = 370;
+
+percemon::Expr get_phi1() {
+  using namespace percemon;
+
+  auto id1 = Var_id{"1"};
+  auto id2 = Var_id{"2"};
+  Expr phi = Forall({id1})->dot(
+      Expr{Previous(Const{true})} >>
+      Previous(Exists({id2})->dot((id1 == id2) & Expr{Class(id1) == Class(id2)})));
+
+  return phi;
+}
+
+percemon::Expr get_phi2() {
+  using namespace percemon;
+
+  auto id1 = Var_id{"1"};
+  auto id2 = Var_id{"2"};
+  auto x   = Var_x{"1"};
+  auto f   = Var_f{"1"};
+
+  Expr phi1 = And({1 <= f - C_FRAME{}, f - C_FRAME{} <= 2});
+  Expr phi2 = Exists({id2})->dot((id1 == id2) & Expr{Class(id1) == Class(id2)});
+
+  Expr phi = Forall({id1})->at({x, f})->dot(Always(phi1 >> phi2));
+  return phi;
+}
+}  // namespace
+
+namespace percemon_ros {
+
+struct Monitor : public rclcpp::Node {
+ private:
+  /// Bounding boxes subscriber
+  rclcpp::Subscription<perception_interfaces::msg::BoundingBoxes>::SharedPtr m_bbox_sub;
+
+  /// Satisfaction msg publisher
+  rclcpp::Publisher<monitoring_interfaces::msg::SatisfactionArray>::SharedPtr m_sat_pub;
+
+  /// Monitor
+  percemon::monitoring::OnlineMonitor m_monitor1;
+  percemon::monitoring::OnlineMonitor m_monitor2;
+
+  size_t m_frame_num = 0;
+
+ public:
+  Monitor() :
+      rclcpp::Node("perception_monitor"),
+      m_monitor1{get_phi1(), 10.0, IMG_WIDTH, IMG_HEIGHT},
+      m_monitor2{get_phi2(), 10.0, IMG_WIDTH, IMG_HEIGHT} {
+    RCLCPP_INFO(get_logger(), "*****************************");
+    RCLCPP_INFO(get_logger(), " Perception Monitoring Node ");
+    RCLCPP_INFO(get_logger(), "*****************************");
+    RCLCPP_INFO(get_logger(), " * namespace: %s", get_namespace());
+    RCLCPP_INFO(get_logger(), " * node name: %s", get_name());
+    RCLCPP_INFO(get_logger(), "*****************************");
+
+    this->m_bbox_sub =
+        this->create_subscription<perception_interfaces::msg::BoundingBoxes>(
+            "/object_detector/detections", 10, [this](const auto bbox) -> void {
+              this->handle_bbox(bbox);
+            });
+
+    this->m_sat_pub =
+        this->create_publisher<monitoring_interfaces::msg::SatisfactionArray>(
+            "/perception_monitor/satisfaction", 10);
+  }
+
+  void handle_bbox(const perception_interfaces::msg::BoundingBoxes::SharedPtr msg) {
+    auto frame      = percemon::datastream::Frame{};
+    frame.timestamp = rclcpp::Time{msg->header.stamp}.seconds();
+    frame.frame_num = this->m_frame_num++;
+    frame.size_x    = msg->width;
+    frame.size_y    = msg->height;
+    for (const auto& bbox : msg->bounding_boxes) {
+      std::string id    = bbox.class_label;
+      auto obj          = percemon::datastream::Object{};
+      obj.object_class  = bbox.class_id;
+      obj.probability   = bbox.probability;
+      obj.bbox.xmax     = bbox.x + bbox.width;
+      obj.bbox.xmin     = bbox.x;
+      obj.bbox.ymin     = bbox.y;
+      obj.bbox.ymax     = bbox.y + bbox.height;
+      frame.objects[id] = obj;
+    }
+
+    this->m_monitor1.add_frame(frame);
+    this->m_monitor2.add_frame(frame);
+
+    auto ret     = monitoring_interfaces::msg::SatisfactionArray{};
+    auto header  = msg->header;
+    header.stamp = this->get_clock()->now();
+    ret.header   = header;
+    auto values  = std::vector<bool>{};
+    values.push_back(m_monitor1.eval() >= 0);
+    values.push_back(m_monitor2.eval() >= 0);
+    ret.values = std::move(values);
+
+    this->m_sat_pub->publish(ret);
+  }
+};
+
+}  // namespace percemon_ros
